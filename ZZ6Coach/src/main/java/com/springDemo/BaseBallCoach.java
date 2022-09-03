435df284b67f8d53d18dc4c5465b326aff9b58c3
akshaynirmal19 <110170201+akshaynirmal19@users.noreply.github.com>
2022-09-03 13:02:00 +0530

    Add files via upload

---
+++ ZZ6Coach/src/main/java/com/springDemo/BaseBallCoach.java
@@ -0,0 +1,9 @@
+package com.springDemo;
+
+public class BaseBallCoach implements Coach {
+	
+	public String getDailyWorkout() {
+		return "Spent 30 minute on Ground";
+	}
+
+}
---
+++ ZZ6Coach/src/main/java/com/springDemo/TrackCoach.java
@@ -0,0 +1,10 @@
+package com.springDemo;
+
+public class TrackCoach implements Coach {
+
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Run a 5km";
+	}
+
+}
